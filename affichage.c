2cd042838ebf09f174e0ad41b0e807065ede7c8e
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-13 10:44:30 +0200

    Update with new functions
    alpha_to_indice, input_into_matrice and verif_legal_input

--- affichage.c
+++ affichage.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "affichage.h"
 
-char alph[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+#define TRUE 1
+#define FALSE 0
+char alph[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
 
 void affichage_matrice(int** tab, int taille)
 /*
@@ -48,4 +50,38 @@ void affichage_matrice(int** tab, int taille)
     printf("+---");
   }
   printf("+\n");
+}
+
+int alpha_to_indice(char c, int taille)
+{
+  int y = -1;
+  for(int i = 0; i<taille; i++)
+    {
+      if (alph[i] == c)
+      {
+        y = i;
+      }
+    }
+  return y;
+}
+
+int verif_legal_input(int taille, int x, int y, int val)
+{
+  if(((y >= 0) && (y < taille)) && ((x >= 0) && (x < taille)) && ((val == 0) || (val == 1)))
+  {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+int input_into_matrice(int**tab, int x, char y_char, int val, int taille)
+{
+  int y = alpha_to_indice(y_char, taille);
+  x--;
+  if ((y != -1) && (verif_legal_input(taille, x, y, val) == TRUE))
+  {
+    tab[x][y] = val;
+    return TRUE;
+  }
+  return FALSE;
 }
--- affichage.h
+++ affichage.h
@@ -2,5 +2,8 @@
 #define LAB_AFFICHAGE_H
 
 void affichage_matrice(int** tab, int taille);
+int alpha_to_indice(char c, int taille);
+int input_into_matrice(int**tab, int x, char y_char, int val, int taille);
+int verif_legal_input(int taille, int x, int y, int val);
 
 #endif
