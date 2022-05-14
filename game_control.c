54919b2ccf9222f2a797bf8d8e4fa7897b00d7e7
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-14 22:30:12 +0200

    Update game_control.c

--- game_control.c
+++ game_control.c
@@ -3,7 +3,8 @@
 
 #define TRUE 1
 #define FALSE 0
-char alph[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+char alpha_maj[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+char alpha_min[16] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
 
 void affichage_matrice(int** tab, int taille)
 /*
@@ -18,7 +19,7 @@ void affichage_matrice(int** tab, int taille)
 {
   for(int i=0; i<taille; i++)
   {
-    printf("\t%c", alph[i]);
+    printf("\t%c", alpha_maj[i]);
   }
   printf("\n");
 
@@ -53,19 +54,48 @@ void affichage_matrice(int** tab, int taille)
 }
 
 int alpha_to_indice(char c, int taille)
+/*
+ * Fonction: alpha_to_indice
+ * -----------------
+ * Permet de convertir l'input du joueur de la lettre colonne en sa position dans l'alphabet
+ *
+ * c: charactère
+ * taille: taille du tableau
+ *
+ * return: y (index de c dans alpha). Si lettre non correspondante
+ *
+ */
 {
   int y = -1;
   for(int i = 0; i<taille; i++)
     {
-      if (alph[i] == c)
+      if (alpha_maj[i] == c)
       {
         y = i;
+      }
+      else if (alpha_min[i] == c)
+      {
+          y = i;
       }
     }
   return y;
 }
 
 int verif_legal_input(int taille, int x, int y, int val)
+/*
+ * Fonction: verif_legal_input
+ * -----------------
+ * Permet de vérifier si l'input est légal ou non
+ * Conditions: x et y doivent être positif et inférieur à la taille du tableau, val doit être égale à 0 ou 1
+ *
+ * taille: taille du tableau
+ * x: position x de val
+ * y: position y de val
+ * val: valeur choisie par l'utilisateur (0 ou 1)
+ *
+ * return: boolean en fonction de condition respectée ou non
+ *
+ */
 {
   if(((y >= 0) && (y < taille)) && ((x >= 0) && (x < taille)) && ((val == 0) || (val == 1)))
   {
@@ -75,6 +105,22 @@ int verif_legal_input(int taille, int x, int y, int val)
 }
 
 int input_into_matrice(int**tab, int x, char y_char, int val, int taille)
+/*
+ * Fonction: input_into_matrice
+ * -----------------
+ * Injecte la valeur dans le Takuzu
+ * Convertie la valeur y_char qui est un input de type char en sa position dans l'alphabet par la fonction alpha_to_indice()
+ * Condition: Vérifie tout d'abord avec la fonction verif_legal_input() si la valeur est autorisée ou non.
+ *
+ * **tab: pointeur de tableau
+ * x: position x de val
+ * y_char: position y de val (lettre correspondante)
+ * val: valeur choisie par l'utilisateur (0 ou 1)
+ * taille: taille du tableau
+ *
+ * return: boolean en fonction de condition respectée ou non
+ *
+ */
 {
   int y = alpha_to_indice(y_char, taille);
   x--;
