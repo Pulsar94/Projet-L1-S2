a4751f284d53ddc8967471cad3ecf64343028fbf
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-15 23:46:08 +0200

    clean .h

--- matrice.h
+++ matrice.h
@@ -6,10 +6,10 @@ int** creation_mat_modele(int);
 int** creation_matrice(int);
 void libere_matrice(int, int**);
 int verification_cote(int, int, int, int, int**);
-int** generation_solution(int);
+int** generation_solution(int, int**);
 int** creer_masque_aleat(int);
 int** creer_masque_spe(int);
-int input_into_masque(int**tab, int x, char y_char, int val, int taille);
+int input_into_masque(int**, int, char, int, int);
 int** takuzu_utilisateur(int**, int**, int, int);
 
 #endif //PROJET_L1_S2_MATRICE_H
--- regles_taku.h
+++ regles_taku.h
@@ -9,12 +9,12 @@
 int verification_cote (int, int, int, int, int**);
 int verification_lig_col(int, int, int, int**);
 int verification_nb_iden(int, int, int, int, int**);
-int verif_regles_taku(int** tab_game, int x, int y, int val, int taille, int*);
-int grille_pleine(int** tab_sol, int**tab_game, int taille);
+int verif_regles_taku(int**, int, int, int, int, int*);
+int grille_pleine(int**, int**, int);
 int* indice_init();
-void supprime_indice(int* indice);
-void recherche_indice(int* indice, int player);
-void modif_indice(int* indice, int index_indice, int val);
+void supprime_indice(int*);
+void recherche_indice(int*, int);
+void modif_indice(int*, int, int);
 int verification_ligne_sol(int*, int);
 int verification_colonne_sol(int**, int);
 int verification_nombre_sol_col(int, int**);
